fbce1c210b3868a8d4cadc90dc4d0e169daaa31a
CesarVS <cesar8vs@gmail.com>
2025-09-29 22:51:16 -0500

    Mejoras adicionales

---
+++ src/main/resources/static/js/home.js
@@ -0,0 +1,4 @@
+
+document.getElementById("go-to-senda").addEventListener("click", function() {
+    window.location.href = "../templates/library.html"; 
+  });
--- src/main/resources/static/js/library.js
+++ src/main/resources/static/js/library.js
@@ -2,8 +2,15 @@ console.log("Hola mundo");
 document.createElement("cardBook-template");
 
 const cardBookTemplate = document.getElementById('cardBook-template');
-const card = cardBookTemplate.content.cloneNode(true);
 
-document.getElementById('container-cards').appendChild(card);
+
+
+for (let index = 0; index < 3; index++) {
+    var cardnew = cardBookTemplate.content.cloneNode(true);
+    document.getElementById('container-cards').appendChild(cardnew);
+    
+}
+
+
 
 
