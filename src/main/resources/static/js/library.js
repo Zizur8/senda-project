ec096b780aff654dda45f401e117803fb2808903
CesarVS <cesar8vs@gmail.com>
2025-10-02 09:41:36 -0500

    Cambios drasticos al formulario de crear

--- src/main/resources/static/js/library.js
+++ src/main/resources/static/js/library.js
@@ -1,5 +1,14 @@
-console.log("Hola mundo");
-document.createElement("cardBook-template");
+
+const formNewBookTemplate = document.getElementById('form-new-book');
+const buttonShowFormNewBook = document.getElementById('addBook');
+
+    const main = document.querySelector('main');
+    
+        const formnew = formNewBookTemplate.content.cloneNode(true);
+        main.appendChild(formnew);
+
+
+buttonShowFormNewBook.addEventListener('click', openFormNewBook);
 
 const cardBookTemplate = document.getElementById('cardBook-template');
 
@@ -9,6 +18,15 @@ for (let index = 0; index < 3; index++) {
     var cardnew = cardBookTemplate.content.cloneNode(true);
     document.getElementById('container-cards').appendChild(cardnew);
     
+}
+
+
+
+function openFormNewBook() {
+    const main = document.querySelector('main');
+    
+        const formnew = formNewBookTemplate.content.cloneNode(true);
+        main.appendChild(formnew);
 }
 
 
